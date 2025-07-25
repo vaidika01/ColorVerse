0fb33cb05cfc8af62a4af8d78d7b0e73d0e065f6
Vaidika <136914466+vaidika01@users.noreply.github.com>
2025-07-26 05:06:30 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://colorverse-frontend.onrender.com/"],
+    origin: ["https://colorverse-frontend.onrender.com/", "http://localhost:5173"],
     credentials: true,
   })
 );
